909162f026f56fe48bf051e98cf97644bcc72c39
Rishi Khan <rishi.rkhan@gmail.com>
2025-01-15 22:23:34 -0400

    feat(eslint): add eslint configuration to project for code quality and consistency feat(prettier): add prettier configuration to project for code formatting consistency fix(database.ts): update rejectUnauthorized value to false for SSL connection feat(middlewares.ts): add security middleware configuration for content security policy feat(plugins.ts): add upload plugin configuration for strapi-provider-upload-supabase feat(package.json): update devDependencies for eslint, typescript, and prettier feat(landing-page): add landing page content type, controller, routes, and service feat(social-link): add social link content type, controller, routes, and service feat(theme): add theme content type, controller, routes, and service
    feat(theme.ts): add a new theme service file to handle theme-related functionality
    
    refactor(contentTypes.d.ts): simplify attribute formatting for createdBy, updatedBy,
    and localizations for better readability
    feat(contentTypes.d.ts): add support for private attributes in various interfaces to
    enhance data security and privacy
    feat(contentTypes.d.ts): introduce new interfaces ApiLandingPageLandingPage,
    ApiSocialLinkSocialLink, and ApiThemeTheme with draftAndPublish options for
    future content management capabilities
    
    refactor(contentTypes.d.ts): reformat code for better readability and consistency in attribute definitions
    
    refactor(contentTypes.d.ts): reformat code for better readability and consistency in attribute definitions

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,32 @@
-export default [
+export default ({ env }) => [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  /* Beginning of snippet */
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:', 'http:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'res.cloudinary.com', // cloudinary images
+            'lh3.googleusercontent.com', // google avatars
+            'platform-lookaside.fbsbx.com', // facebook avatars
+            'dl.airtable.com', // strapi marketplace,
+            'market-assets.strapi.io',
+            env('SUPABASE_API_URL'),
+          ],
+          'media-src': ["'self'", 'data:', 'blob:', env('SUPABASE_API_URL')],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  /* End of snippet */
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,13 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'strapi-provider-upload-supabase',
+      providerOptions: {
+        apiUrl: env('SUPABASE_API_URL'),
+        apiKey: env('SUPABASE_API_KEY'),
+        bucket: env('SUPABASE_AWS_BUCKET'),
+        directory: env('SUPABASE_AWS_DIRECTORY'),
+      },
+    },
+  },
+});
