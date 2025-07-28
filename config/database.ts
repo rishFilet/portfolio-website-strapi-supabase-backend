9447eb2b1b9b84178b5f50318264e9011f2be050
Rishi Khan <rishi.rkhan@gmail.com>
2025-07-28 13:54:11 -0400

    refactor(database.ts): update pool configuration for postgres connection to improve performance and resource management feat(package.json): update pg package to version 8.16.3 for compatibility with the latest changes feat(package.json): add supabase-js package to dependencies for integrating with Supabase services refactor(theme/schema.json): simplify allowedTypes array structure for media fields to improve readability and maintainability

--- config/database.ts
+++ config/database.ts
@@ -24,23 +24,22 @@ export default ({ env }) => {
     },
     postgres: {
       connection: {
-        connectionString: env('SUPABASE_URL'),
         host: env('SUPABASE_HOST', 'localhost'),
         port: env.int('SUPABASE_PORT', 5432),
         database: env('SUPABASE_NAME', 'strapi'),
         user: env('SUPABASE_USERNAME', 'strapi'),
         password: env('SUPABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
-        },
+        ssl: env.bool('DATABASE_SSL', false) ? 'prefer' : false,
         schema: env('DATABASE_SCHEMA', 'public'),
       },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+      pool: {
+        min: 0,
+        max: 1,
+        acquireTimeoutMillis: 30000,
+        createTimeoutMillis: 30000,
+        destroyTimeoutMillis: 5000,
+        idleTimeoutMillis: 30000,
+      },
     },
     sqlite: {
       connection: {
@@ -54,7 +53,7 @@ export default ({ env }) => {
     connection: {
       client,
       ...connections[client],
-      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 10000),
     },
   };
 };
